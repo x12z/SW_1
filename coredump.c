9b9710c98162ab5e3d65538879de86ac73cccfdd
x12z <scps9702035633@gmail.com>
2021-09-21 16:26:32 +0000

    coredump.c

---
+++ coredump.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void test()
+{
+    char* s = "abc";
+    //*s = 'x';
+}
+
+int main(int argc, char** argv)
+{
+    test();
+    return (EXIT_SUCCESS);
+}
